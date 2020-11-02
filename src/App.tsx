6ae3da938538eab60c28996ac9f34ded2db8d8d9
Никита Соболев <nikita@MacBook-Pro-Nikita.local>
2020-11-02 15:57:45 +0300

    Add eslint and Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,8 @@ import React from 'react';
 import styles from './App.module.less';
 import './custom.less';
 
-export const App = () => {
-  return (
-    <div className={`${styles.header} color`}>
-      This is App Component!
-    </div>
-  )
+const App = () => {
+  return <div className={`${styles.header} color`}>This is App Component!</div>;
 };
+
+export default App;
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import './index.less';
 
-import { App } from './App';
+import App from './App';
 
 const root = document.querySelector('#root');
 
