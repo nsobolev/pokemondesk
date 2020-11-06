a0703bace708309df8220506f8adce8e3a9e4105
Никита Соболев <nikita@MacBook-Pro-Nikita.local>
2020-11-06 21:27:08 +0300

    Add component - ParallaxImage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
+
 import styles from './Home.module.less';
+import PokemonsImageUrl from './assets/main-background.png';
 
 import * as Button from '../../components/Button';
+import ParallaxImage from '../../components/ParallaxImage';
 
 const HomePage = () => {
   return (
     <div className={styles.home}>
-      <div className={styles.home__wrapper}>
-        <div className={`${styles.home__container} container`}>
-          <div className={styles.home__content}>
-            <div className={styles.home__information}>
-              <h2 className={styles.home__slogan}>Find all your favorite Pokemon</h2>
-              <p className={styles.home__description}>
-                You can know the type of Pokemon, its strengths, disadvantages and abilities
-              </p>
-              <Button.ViewLink color={Button.ColorsButton.Success} to="/pokemons">
-                See pokemons
-              </Button.ViewLink>
-            </div>
+      <div className={`${styles.home__container} container`}>
+        <div className={styles.home__content}>
+          <div className={styles.home__information}>
+            <h2 className={styles.home__slogan}>Find all your favorite Pokemon</h2>
+            <p className={styles.home__description}>
+              You can know the type of Pokemon, its strengths, disadvantages and abilities
+            </p>
+            <Button.ViewLink color={Button.ColorsButton.Success} to="/pokemons">
+              See pokemons
+            </Button.ViewLink>
+          </div>
+          <div className={styles.home__gallery}>
+            <ParallaxImage url={PokemonsImageUrl} alt="Pikachu and pokemons" offsetX={0.04} offsetY={0.05} />
           </div>
         </div>
       </div>
